f8594a11e2f49a6a5054cb3419a5fe5af1a41d79
Andreas Knab <knabar@gmail.com>
2008-11-12 22:42:20 +0000

    Added simple group list view

--- rooibos/data/urls.py
+++ rooibos/data/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.contrib.syndication.views import feed
+from django.views.generic.simple import direct_to_template
 from views import *
 from feeds import GroupFeed
 
@@ -8,6 +9,7 @@ feeds = {
 }
 
 urlpatterns = patterns('',
+    url(r'^groups/$', groups, name='data-groups'),
     url(r'^group/(?P<groupname>[-\w]+)/$', group_raw, name='data-group'),
     url(r'^record/(?P<recordname>[-\w]+)/$', record_raw, name='data-record'),
     (r'^feeds/(?P<url>.*)/$', feed, {'feed_dict': feeds}),
--- rooibos/data/views.py
+++ rooibos/data/views.py
@@ -2,17 +2,22 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, get_list_or_404, render_to_response
 from django.template import RequestContext
 from models import Group, Record
+from rooibos.access.views import filter_by_access
+
+def groups(request):    
+    groups = filter_by_access(request.user, Group.objects.all())    
+    return render_to_response('data_groups.html',
+                              {'groups': groups, },
+                              context_instance=RequestContext(request))
 
 def group_raw(request, groupname):
-    group = get_object_or_404(Group, name=groupname)
-    
+    group = get_object_or_404(Group, name=groupname)    
     return render_to_response('data_group.html',
                               {'group': group, },
                               context_instance=RequestContext(request))
 
 def record_raw(request, recordname):
-    record = get_object_or_404(Record, name=recordname)
-    
+    record = get_object_or_404(Record, name=recordname)    
     return render_to_response('data_record.html',
                               {'record': record, },
                               context_instance=RequestContext(request))
