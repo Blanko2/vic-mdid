c0e34ba0ffd83866c8bcad7e022d122e1eef4e46
Andreas Knab <knabar@gmail.com>
2008-11-27 03:49:25 +0000

    Added *nix resetdb script, moved settings file

---
+++ rooibos/resetdb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo You are about to delete your database, please cancel if this is not what you want!
+read
+mysqladmin -u root --force drop rooibos
+mysqladmin -u root --default-character-set=utf8 create rooibos
+python manage.py syncdb --noinput
+python manage.py createcachetable cache
