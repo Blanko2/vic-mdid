3f03f356cee3edd413a90e627a0dbd93c0144be9
Andreas Knab <knabar@gmail.com>
2008-12-02 15:48:13 +0000

    Added effective permissions display for user

--- rooibos/access/urls.py
+++ rooibos/access/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 from views import *
 
 urlpatterns = patterns('',
-    url(r'^view/(?P<app_label>[\w-]+)/(?P<model>[\w-]+)/(?P<object>[\w-]+)/$', access_view, name='access_view'),
+    url(r'^view/(?P<app_label>[\w-]+)/(?P<model>[\w-]+)/(?P<object>[\w-]+)/(?:for/(?P<foruser>.+)/)?$',
+        access_view, name='access_view'),
 )
--- rooibos/access/views.py
+++ rooibos/access/views.py
@@ -3,23 +3,27 @@ from django.shortcuts import get_object_or_404, get_list_or_404, render_to_respo
 from django.template import RequestContext
 from django.db.models import Q
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from models import AccessControl
-from . import get_effective_permissions
+from . import check_access, get_effective_permissions
 
-
-def access_view(request, app_label, model, object):
-    
-    model = ContentType.objects.get(app_label=app_label, model=model)
+def access_view(request, app_label, model, object, foruser=None):
     try:
-        object = model.get_object_for_this_type(name=object)
+        model = ContentType.objects.get(app_label=app_label, model=model)
+        object = model.get_object_for_this_type(name=object)        
     except ObjectDoesNotExist:
         raise Http404
-    (r, w, m) = get_effective_permissions(request.user, object)
-    if not m:
-        raise Http404    
+    check_access(request.user, object, manage=True, fail_if_denied=True)
     rules = AccessControl.objects.filter(content_type__id=model.id, object_id=object.id)
+    if foruser:
+        foruser = get_object_or_404(User, username=foruser)
+        foruser_acl = get_effective_permissions(foruser, object)
+    else:
+        foruser_acl = None
     return render_to_response('access_view.html',
                               {'object': object,
-                               'rules': rules,},
+                               'rules': rules,
+                               'foruser': foruser,
+                               'foruser_acl': foruser_acl,},
                               context_instance=RequestContext(request))
