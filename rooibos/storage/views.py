5f81443e2f91e2443ef101c566b727a48a7163f7
Andreas Knab <knabar@gmail.com>
2008-12-03 22:18:20 +0000

    Media access control fix

--- rooibos/storage/views.py
+++ rooibos/storage/views.py
@@ -7,10 +7,10 @@ from models import Media, Storage
 
 @cache_control(max_age=3600)
 def retrieve(request, record, media):
-    mediaobj = get_object_or_404(Media, name=media,
+    mediaobj = get_object_or_404(Media.objects.filter(name=media,
                                  record__name=record,
                                  record__group__id__in=accessible_ids(request.user, Group),
-                                 storage__id__in=accessible_ids(request.user, Storage))
+                                 storage__id__in=accessible_ids(request.user, Storage)).distinct())
     content = mediaobj.load_file()
     if content:
         return HttpResponse(content=content, mimetype=str(mediaobj.mimetype))
