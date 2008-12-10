7741283856e190b96b07b2e78b7be0a2468904a0
Andreas Knab <knabar@gmail.com>
2008-12-10 02:25:23 +0000

    Fixed database reset script

--- rooibos/resetdb.sh
+++ rooibos/resetdb.sh
@@ -2,6 +2,6 @@
 echo You are about to delete your database, please cancel if this is not what you want!
 read
 mysqladmin -u root --force drop rooibos
-mysqladmin -u root --default-character-set=utf8 create rooibos
+mysql -u root -e "CREATE DATABASE rooibos CHARACTER SET utf8"
 python manage.py syncdb --noinput
 python manage.py createcachetable cache
