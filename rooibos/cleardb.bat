14ce9ec3bc024cc82e378d3e6e8302e062ff1e2c
Andreas Knab <knabar@gmail.com>
2009-05-27 19:19:47 +0000

    Fixed issues with cleardb and dumpdata batch files

--- rooibos/cleardb.bat
+++ rooibos/cleardb.bat
@@ -1,1 +1,3 @@
-mysqldump -u root --add-drop-table --no-data rooibos | mysql -u root rooibos
+mysqldump -u root --add-drop-table --no-data rooibos > _temp_rooibos_structure.mysql
+mysql -u root rooibos < _temp_rooibos_structure.mysql
+del _temp_rooibos_structure.mysql
--- rooibos/dumpdata.bat
+++ rooibos/dumpdata.bat
@@ -1,1 +1,2 @@
+@echo off
 python manage.py dumpdata --format=json --indent=2 -e sessions
