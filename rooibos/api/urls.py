b5f5c7bb437375e5abe45b973edc8cf544606677
Scott Stults <sstults@opensourceconnections.com>
2009-07-16 21:07:50 +0000

    Initial checkin of JSON api

---
+++ rooibos/api/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import *
+
+from views import *
+
+urlpatterns = patterns('',
+    url(r'^about/$', about),
+    url(r'^collections(/(?P<id>\d+))?$', collections),
+    url(r'^login/$', login),
+    url(r'^logout/$', logout),
+    url(r'^search/$', search),
+    url(r'^search(/(?P<id>\d+)/(?P<name>[\w-]+))?/$', search),
+    url(r'^record/(?P<id>\d+)/(?P<name>[-\w]+)/$', record),
+)
