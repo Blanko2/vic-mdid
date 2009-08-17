1f80f6df91c16e3de572e409444af35df4d9acb1
Andreas Knab <knabar@gmail.com>
2009-08-17 18:25:51 +0000

    Added simple API calls to retrieve presentations

--- rooibos/api/urls.py
+++ rooibos/api/urls.py
@@ -10,4 +10,6 @@ urlpatterns = patterns('',
     url(r'^search/$', search),
     url(r'^search(/(?P<id>\d+)/(?P<name>[\w-]+))?/$', search),
     url(r'^record/(?P<id>\d+)/(?P<name>[-\w]+)/$', record),
+    url(r'^presentations/currentuser/$', presentations_for_current_user),
+    url(r'^presentation/(?P<id>\d+)/$', presentation_detail),
 )
--- rooibos/api/views.py
+++ rooibos/api/views.py
@@ -5,17 +5,18 @@ from django.http import HttpResponse, HttpRequest
 from django.utils import simplejson
 from django.core import serializers
 from rooibos.ui import update_record_selection
-from rooibos.util import safe_int
+from rooibos.util import safe_int, json_view
 from rooibos.access import filter_by_access, accessible_ids, accessible_ids_list
 from rooibos.storage.models import Storage, Media
 from rooibos.data.models import Field, Collection, FieldValue, Record
 from rooibos.solr.views import *
 from rooibos.data.models import *
 from rooibos.storage import get_thumbnail_for_record
+from rooibos.presentation.models import Presentation
 
 from rooibos.access import filter_by_access
 from django.conf import settings
-   
+
 def about(request):
     return render_to_response('main.html',
                               {},
@@ -40,7 +41,7 @@ def login(request):
         login(request, user)
         resp='{"sessionid" : '+request.session.session_key+'}'
     return HttpResponse(resp, content_type='application/javascript')
-    
+
 def logout(request):
     from django.contrib.auth import logout
     logout(request)
@@ -49,11 +50,11 @@ def logout(request):
 
 def search(request, id=None, name=None, selected=False):
     collection = id and get_object_or_404(filter_by_access(request.user, Collection), id=id) or None
-    
+
     update_record_selection(request)
-    
+
     templates = dict(l='list')
-    
+
     viewmode = request.GET.get('v', 'thumb')
     if viewmode == 'l':
         pagesize = max(min(safe_int(request.GET.get('ps', '100'), 100), 200), 5)
@@ -67,19 +68,19 @@ def search(request, id=None, name=None, selected=False):
     remove = request.GET.get('rem', None)
     if remove: criteria.remove(remove)
     keywords = request.GET.get('kw', '')
-    
+
     if request.GET.has_key('action'):
         page = safe_int(request.GET.get('op', '1'), 1)
-    
+
     if selected:
         selected = request.session.get('selected_records', ())
-    
+
     available_storage = accessible_ids_list(request.user, Storage)
-    
+
     exclude_facets = ['identifier']
 
     fields = Field.objects.filter(standard__prefix='dc').exclude(name__in=exclude_facets)
- 
+
     search_facets = [SearchFacet('tag', 'Tags')] + [SearchFacet(field.name + '_t', field.label) for field in fields]
     search_facets.append(StorageSearchFacet('resolution', 'Image size', available_storage))
     search_facets.append(StorageSearchFacet('mimetype', 'Media type', available_storage))
@@ -88,11 +89,11 @@ def search(request, id=None, name=None, selected=False):
     search_facets = dict((f.name, f) for f in search_facets)
 
     query = _generate_query(search_facets, request.user, collection, criteria, keywords, selected, remove)
-       
+
     s = SolrIndex()
-    
+
     return_facets = search_facets.keys()
-    
+
     (hits, records, facets) = s.search(query, sort=sort, rows=pagesize, start=(page - 1) * pagesize,
                                        facets=return_facets, facet_mincount=1, facet_limit=100)
 
@@ -112,7 +113,7 @@ def record(request, id, name):
     media = Media.objects.select_related().filter(record=record, storage__id__in=accessible_ids(request.user, Storage))
 
     fieldsets = FieldSet.objects.filter(Q(owner=request.user) | Q(standard=True)).order_by('title')
-    
+
     selected_fieldset = request.GET.get('fieldset')
     if selected_fieldset == '_all':
         fieldset = None
@@ -122,10 +123,10 @@ def record(request, id, name):
             fieldset = f[0]
         else:
             fieldset = record.fieldset
-            selected_fieldset = None            
+            selected_fieldset = None
     else:
         fieldset = record.get(id=id).fieldset
-    
+
     fieldvalues = record.get(id=id).get_fieldvalues(fieldset=fieldset)
 
     dict =  {'record': serializers.serialize("json", record),
@@ -136,7 +137,7 @@ def record(request, id, name):
              }
 
     return HttpResponse(simplejson.dumps(dict), content_type='application/javascript')
-    
+
 def _generate_query(search_facets, user, collection, criteria, keywords, selected, *exclude):
 
     fields = {}
@@ -147,16 +148,16 @@ def _generate_query(search_facets, user, collection, criteria, keywords, selecte
         if f.startswith('-'):
             f = 'NOT ' + f[1:]
         fname = f.rsplit(' ',1)[-1]
-        
+
         # create exact match criteria on the fly if needed
         if fname.endswith('_s') and not search_facets.has_key(fname):
             search_facets[fname] = ExactValueSearchFacet(fname)
-        
+
         o = search_facets[fname].process_criteria(o, user)
         fields.setdefault(f, []).append('(' + o.replace('|', ' OR ') + ')')
     fields = map(lambda (name, crit): '%s:(%s)' % (name, (name.startswith('NOT ') and ' OR ' or ' AND ').join(crit)),
                  fields.iteritems())
-    
+
     def build_keywords(q, k):
         k = k.lower()
         if k == 'and' or k == 'or':
@@ -165,13 +166,13 @@ def _generate_query(search_facets, user, collection, criteria, keywords, selecte
             return q + ' ' + k
         else:
             return q + ' AND ' + k
-    
+
     if keywords: keywords = reduce(build_keywords, keywords.split())
-    
+
     query = ''
-    if fields:    
+    if fields:
         query = ' AND '.join(fields)
-    if keywords:    
+    if keywords:
         query = query and '%s AND (%s)' % (query, keywords) or '(%s)' % keywords
     if not query:
         query = '*:*'
@@ -179,7 +180,7 @@ def _generate_query(search_facets, user, collection, criteria, keywords, selecte
         query = 'collections:%s AND %s' % (collection.id, query)
     if selected:
         query = 'id:(%s) AND %s' % (' '.join(map(str, selected)), query)
-        
+
     if not user.is_superuser:
         groups = ' '.join(map(str, accessible_ids_list(user, Collection)))
         c = []
@@ -189,5 +190,50 @@ def _generate_query(search_facets, user, collection, criteria, keywords, selecte
             query = '(%s) AND %s' % (' OR '.join(c), query)
         else:
             query = 'id:"-1"'
-    
+
     return query
+
+
+@json_view
+def presentations_for_current_user(request):
+    presentations = Presentation.objects.filter(owner=request.user).order_by('title')
+    return {
+        'presentations': [
+            dict(id=p.id,
+                 name=p.name,
+                 title=p.title,
+                 hidden=p.hidden,
+                 description=p.description,
+                 created=p.created.isoformat(),
+                 modified=p.modified.isoformat())
+            for p in presentations
+        ]
+    }
+
+
+@json_view
+def presentation_detail(request, id):
+    p = get_object_or_404(Presentation.objects.filter(owner=request.user), id=id)
+    return dict(id=p.id,
+                name=p.name,
+                title=p.title,
+                hidden=p.hidden,
+                description=p.description,
+                created=p.created.isoformat(),
+                modified=p.modified.isoformat(),
+                content=[
+                    dict(
+                        id=item.record.id,
+                        name=item.record.name,
+                        title=item.record.title,
+                        metadata=[
+                            dict(
+                                label=value.resolved_label,
+                                value=value.value
+                                )
+                            for value in item.record.get_fieldvalues(owner=request.user, context=p)
+                        ]
+                    )
+                    for item in p.items.select_related('record')
+                ]
+                 )
