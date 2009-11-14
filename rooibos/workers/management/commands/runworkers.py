a55469050cb9d0308011fac40378bfb2d213ceea
Andreas Knab <knabar@gmail.com>
2009-11-14 18:14:06 +0000

    Added initial gearman worker framework

---
+++ rooibos/workers/management/commands/runworkers.py
@@ -0,0 +1,46 @@
+import os
+from threading import Thread
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from rooibos.workers.registration import create_worker
+from gearman.server import GearmanServer
+from optparse import make_option
+
+
+class Server(Thread):
+
+    def __init__(self, verbosity):
+        super(Server, self).__init__()
+        self.verbosity = verbosity
+
+    def run(self):
+        if self.verbosity > 0:
+            print "Starting server"
+        self.server = GearmanServer(settings.GEARMAN_SERVERS[0])
+        self.server.start()
+
+
+class Command(BaseCommand):
+    help = 'Starts Gearman compatible workers'
+
+    option_list = BaseCommand.option_list + (
+        make_option('--server', action='store_true',
+            help='Run a simple Gearman compatible server'),
+        )
+
+    def handle(self, *commands, **options):
+
+        verbosity = options.get('verbosity', 1)
+        server = options.get('server', False)
+
+        if not settings.GEARMAN_SERVERS:
+            print "No gearman servers configured"
+            return
+
+        if server:
+            Server(verbosity).start()
+
+        worker = create_worker()
+        if verbosity > 0:
+            print "Starting workers: " + ', '.join(worker.abilities.keys())
+        worker.work()
---
+++ rooibos/workers/registration.py
@@ -0,0 +1,43 @@
+import sys
+from django.conf import settings
+from gearman import Task, GearmanWorker, GearmanClient
+
+workers = dict()
+
+client = settings.GEARMAN_SERVERS and GearmanClient(settings.GEARMAN_SERVERS) or None
+
+
+def register_worker(id):
+    def register(worker):
+        workers[id] = worker
+        return worker
+    return register
+
+
+def discover_workers():
+    if not workers:
+        for app in settings.INSTALLED_APPS:
+            try:
+                module = __import__(app + ".workers")
+            except ImportError:
+                pass
+
+
+def create_worker():
+    discover_workers()
+    worker = GearmanWorker(settings.GEARMAN_SERVERS)
+    for id, func in workers.iteritems():
+        worker.register_function(id, func)
+    return worker
+
+
+def run_worker(worker, arg, **kwargs):
+    discover_workers()
+    task = Task(worker, arg, **kwargs)
+    if client:
+        return client.do_task(task)
+    else:
+        if workers.has_key(worker):
+            return workers[worker](task)
+        else:
+            raise NotImplementedError()
