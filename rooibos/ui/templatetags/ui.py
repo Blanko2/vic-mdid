9284c2d9cd77423cc99fccddae127f7ec12a2303
Andreas Knab <knabar@gmail.com>
2010-02-03 21:54:12 +0000

    Template updates Added related pages sidebar item

--- rooibos/ui/templatetags/ui.py
+++ rooibos/ui/templatetags/ui.py
@@ -4,6 +4,7 @@ from django.utils.html import escape
 from django.template.loader import get_template
 from django.template import Context, Variable, Template
 from django.contrib.contenttypes.models import ContentType
+from django.utils import simplejson
 from rooibos.contrib.tagging.models import Tag
 from rooibos.data.models import Record
 from rooibos.util.models import OwnedWrapper
@@ -87,3 +88,35 @@ def tag(context, tag, object=None, removable=False, styles=None):
             'styles': styles,
             'request': context['request'],
             }
+
+
+
+# The following is based on http://www.djangosnippets.org/snippets/829/
+
+class VariablesNode(template.Node):
+    def __init__(self, nodelist, var_name):
+        self.nodelist = nodelist
+        self.var_name = var_name
+        
+    def render(self, context):
+        source = self.nodelist.render(context)
+        context[self.var_name] = simplejson.loads(source)
+        return ''
+
+@register.tag(name='var')
+def do_variables(parser, token):
+    try:
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        msg = '"%s" tag requires arguments' % token.contents.split()[0]
+        raise template.TemplateSyntaxError(msg)
+    m = re.search(r'as (\w+)', arg)
+    if m:
+        var_name, = m.groups()
+    else:
+        msg = '"%s" tag had invalid arguments' % tag_name
+        raise template.TemplateSyntaxError(msg)
+           
+    nodelist = parser.parse(('endvar',))
+    parser.delete_first_token()
+    return VariablesNode(nodelist, var_name)
