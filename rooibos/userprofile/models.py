b193ddcffc4acde04cb393cf765d30300137562a
Andreas Knab <knabar@gmail.com>
2010-03-12 20:34:43 +0000

    Work on basket

--- rooibos/userprofile/models.py
+++ rooibos/userprofile/models.py
@@ -6,7 +6,13 @@ class Preference(models.Model):
     setting = models.CharField(max_length=128)
     value = models.TextField()
 
+    def __unicode__(self):
+        return "%s=%s" % (self.setting, self.value)
+    
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
     preferences = models.ManyToManyField(Preference)
+
+    def __unicode__(self):
+        return "%s" % self.user
