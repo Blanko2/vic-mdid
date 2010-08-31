3f993ca4f72fb96c29ac6878f7d0e97140980b0c
Andreas Knab <knab2ar@jmu.edu>
2010-08-31 17:06:47 -0400

    Add template parameter to audio text sync viewer

--- rooibos/viewers/viewers/audiotextsync.py
+++ rooibos/viewers/viewers/audiotextsync.py
@@ -16,35 +16,35 @@ class AudioTextSync(object):
 
     title = "Audio Text Sync"
     weight = 40
-    
+
     def __init__(self):
         pass
-    
+
     def analyze(self, obj, user):
         if not isinstance(obj, Record):
             return NO_SUPPORT
-        
+
         storages = filter_by_access(user, Storage)
-        
+
         has_text = has_audio = False
         for media in obj.media_set.filter(master=None, storage__in=storages):
             if media.mimetype == 'audio/mpeg':
                 has_audio = True
             elif media.mimetype == 'text/plain':
                 has_text = True
-        
+
         return FULL_SUPPORT if has_audio and has_text else NO_SUPPORT
-    
+
     def url(self):
         return [
             url(r'^audiotextsync/(?P<id>[\d]+)/(?P<name>[-\w]+)/$', self.view, name='viewers-audiotextsync'),
             url(r'^audiotextsync/(?P<id>[\d]+)/(?P<name>[-\w]+)/setmarker/$', self.set_marker, name='viewers-audiotextsync-setmarker'),
             ]
-    
+
     def url_for_obj(self, obj):
         return reverse('viewers-audiotextsync', kwargs={'id': obj.id, 'name': obj.name})
-    
-    
+
+
     def _get_record_and_access(self, request, id, name):
         record = Record.get_or_404(id, request.user)
         can_edit = request.user.is_authenticated() and (
@@ -52,9 +52,9 @@ class AudioTextSync(object):
             check_access(request.user, record, write=True) or
             # or if user has write access to collection:
             accessible_ids(request.user, record.collection_set, write=True).count() > 0)
-        return (record, can_edit)        
-    
-    
+        return (record, can_edit)
+
+
     def _get_markers(self, record):
         markers, created = record.fieldvalue_set.get_or_create(
             field=get_system_field(),
@@ -64,12 +64,12 @@ class AudioTextSync(object):
             )
         )
         return markers
-    
-    def view(self, request, id, name):
+
+    def view(self, request, id, name, template='audiotextsync/audiotextsync.html'):
         next = request.GET.get('next')
         record, can_edit = self._get_record_and_access(request, id, name)
         storages = filter_by_access(request.user, Storage)
-        
+
         textmedia = audiomedia = None
         for media in record.media_set.filter(master=None, storage__in=storages):
             if not audiomedia and media.mimetype == 'audio/mpeg':
@@ -78,11 +78,11 @@ class AudioTextSync(object):
                 textmedia = media
         if not textmedia or not audiomedia:
             raise Http404()
-            
+
         transcript = textmedia.load_file().readlines()
         markers = self._get_markers(record)
 
-        return render_to_response('audiotextsync/audiotextsync.html',
+        return render_to_response(template,
                                   {'record': record,
                                    'next': next,
                                    'transcript': transcript,
@@ -91,8 +91,8 @@ class AudioTextSync(object):
                                    'edit': can_edit,
                                    },
                                   context_instance=RequestContext(request))
-    
-    
+
+
     def set_marker(self, request, id, name):
         @json_view
         def json_set_marker(request, id, name):
@@ -122,4 +122,3 @@ class AudioTextSync(object):
             else:
                 return dict(result="Error", message="Invalid method. Use POST.")
         return json_set_marker(request, id, name)
-        
