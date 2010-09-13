3a9eb6050069346615cc5ac75e769d28dca9279d
Andreas Knab <knab2ar@jmu.edu>
2010-09-13 10:23:39 -0400

    Add settings file for running tests in memory

--- rooibos/manage.py
+++ rooibos/manage.py
@@ -2,16 +2,13 @@
 import os
 import sys
 
-install_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(install_dir)
-sys.path.append(os.path.join(install_dir, 'rooibos', 'contrib'))
-
-from django.core.management import execute_manager
 try:
     import settings # Assumed to be in the same directory.
 except ImportError:
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
+
+from django.core.management import execute_manager
 
 if __name__ == "__main__":
     execute_manager(settings)
---
+++ rooibos/settings_test.py
@@ -0,0 +1,8 @@
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+LOGGING_OUTPUT_ENABLED = DEBUG
+
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = 'test.sqlite'
+
+remove_settings = ['DATABASE_OPTIONS']
