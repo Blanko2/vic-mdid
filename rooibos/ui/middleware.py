f1538931ff4fdfe30330cebf2527ce104fdda37c
Andreas Knab <knab2ar@jmu.edu>
2010-10-07 16:35:37 -0400

    Patch middleware that inserts page title to remove tags from heading

--- rooibos/ui/middleware.py
+++ rooibos/ui/middleware.py
@@ -1,14 +1,16 @@
+from django.utils.html import strip_tags
+
 
 class PageTitles:
-    
+
     def process_view(self, request, view_func, view_args, view_kwargs):
         return None
-    
+
     def process_request(self, request):
         pass
-    
+
     def process_response(self, request, response):
-        
+
         def _find_tag(c, tag):
             start = c.find('<%s>' % tag)
             if start > -1:
@@ -17,15 +19,16 @@ class PageTitles:
                 if end > -1:
                     return (start, end)
             return None
-            
+
         if response.status_code == 200 and response.get('Content-Type', '').startswith('text/html'):
             title = _find_tag(response.content, 'title')
             if title and response.content[title[0]:title[1]] == "MDID":
                 heading = _find_tag(response.content, 'h1')
                 if heading:
-                    response.content = "%sMDID - %s%s" % (response.content[:title[0]],
-                                                          response.content[heading[0]:heading[1]],
-                                                          response.content[title[1]:])
+                    # If the page contains Unicode characters, not converting this to Unicode
+                    # will cause runtime errors as strip_tags forces Unicode.
+                    c = unicode(response.content, 'utf8')
+                    response.content = "%sMDID - %s%s" % (c[:title[0]],
+                                                          strip_tags(c[heading[0]:heading[1]]),
+                                                          c[title[1]:])
         return response
-    
-    
