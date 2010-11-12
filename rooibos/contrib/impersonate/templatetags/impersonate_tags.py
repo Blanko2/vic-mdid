831117a478cb5dda1d28c9f916f4a45d1895dd1d
Andreas Knab <knab2ar@jmu.edu>
2010-11-12 15:19:43 -0500

    Fix context in impersonate app for CSRF token

--- rooibos/contrib/impersonate/templatetags/impersonate_tags.py
+++ rooibos/contrib/impersonate/templatetags/impersonate_tags.py
@@ -1,22 +1,25 @@
 from django import template
 from django.template.loader import render_to_string
 from impersonate.functions import get_real_user, get_available_users
+from django.template import RequestContext
 
 register = template.Library()
 
 
 class ImpersonationFormNode(template.Node):
     def render(self, context):
-        current = get_real_user(context['request'])
-        users = get_available_users(current or context['request'].user.username)
+        request = context['request']
+        current = get_real_user(request)
+        users = get_available_users(current or request.user.username)
         if users:
             return render_to_string('impersonation_form.html',
                                     {'users': users,
                                      'current': current,
-                                     'request': context['request'],})
+                                     'request': request,},
+                                    context_instance=RequestContext(request))
         else:
             return ''
-    
+
 
 @register.tag
 def impersonation_form(parser, token):
