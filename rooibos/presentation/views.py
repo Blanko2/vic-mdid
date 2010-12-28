133e38e4be23537c34d670529dabdefdca70e1b9
Andreas Knab <knab2ar@jmu.edu>
2010-12-28 10:18:46 -0500

    Add support for action bar tagging without JavaScript

--- rooibos/presentation/views.py
+++ rooibos/presentation/views.py
@@ -246,32 +246,32 @@ def browse(request, manage=False):
         if request.POST.get('update_tags'):
             ids = map(int, request.POST.getlist('h'))
             new_tags = parse_tag_input(request.POST.get('new_tags'))
+            all_tags = parse_tag_input(request.POST.get('all_tags'))
             try:
-                update_tags = [(base64.b32decode(k[11:].replace('_', '=')), v)
+                update_tags = dict((base64.b32decode(k[11:].replace('_', '=')), v)
                     for k, v in request.POST.iteritems()
-                    if k.startswith('update_tag_')]
+                    if k.startswith('update_tag_'))
             except TypeError:
                 # Could not decode base32 encoded tag names
                 update_tags = ()
 
+            remove_tags = [tag_name for tag_name in all_tags
+                           if tag_name not in update_tags.keys() and tag_name not in new_tags]
+
             for presentation in presentations.filter(id__in=ids):
                 wrapper = OwnedWrapper.objects.get_for_object(user=request.user, object=presentation)
-                for tag_name, action in update_tags:
+                for tag_name, action in update_tags.iteritems():
                     if action == 'mixed':
                         # Don't need to change anything
                         continue
                     elif action == 'true':
                         # Add tag to all selected presentations
                         Tag.objects.add_tag(wrapper, '"%s"' % tag_name)
-                    elif action == 'false':
-                        # Remove tag from all selected presentations
-                        keep_tags = Tag.objects.get_for_object(wrapper).exclude(name=tag_name).values_list('name')
-                        Tag.objects.update_tags(wrapper,  ' '.join(map(lambda s: '"%s"' % s, keep_tags)))
-                    else:
-                        # Invalid action
-                        pass
                 for tag_name in new_tags:
                     Tag.objects.add_tag(wrapper, '"%s"' % tag_name)
+                for tag_name in remove_tags:
+                    keep_tags = Tag.objects.get_for_object(wrapper).exclude(name=tag_name).values_list('name')
+                    Tag.objects.update_tags(wrapper,  ' '.join(map(lambda s: '"%s"' % s, keep_tags)))
 
         return HttpResponseRedirect(request.get_full_path())
 
