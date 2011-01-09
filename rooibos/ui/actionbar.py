57d16c637f996b67a62a7f22896b61e5c1c2981e
Andreas Knab <knab2ar@jmu.edu>
2011-01-09 10:29:58 -0500

    Split tag input on commas only unless quotes are present

--- rooibos/ui/actionbar.py
+++ rooibos/ui/actionbar.py
@@ -6,7 +6,13 @@ import base64
 
 
 def update_actionbar_tags(request, *objects):
-    new_tags = parse_tag_input(request.POST.get('new_tags'))
+
+    # split tags on commas unless there is a quote in the input
+    nt = request.POST.get('new_tags')
+    if '"' in nt:
+        new_tags = parse_tag_input(nt)
+    else:
+        new_tags = filter(None, map(lambda s: s.strip(), nt.split(',')))
     all_tags = parse_tag_input(request.POST.get('all_tags'))
     try:
         update_tags = dict((base64.b32decode(k[11:].replace('_', '=')), v)
@@ -33,4 +39,3 @@ def update_actionbar_tags(request, *objects):
         for tag_name in remove_tags:
             keep_tags = Tag.objects.get_for_object(wrapper).exclude(name=tag_name).values_list('name')
             Tag.objects.update_tags(wrapper,  ' '.join(map(lambda s: '"%s"' % s, keep_tags)))
-
