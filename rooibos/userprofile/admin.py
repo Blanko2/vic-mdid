fa14c96322aeaf7267327dabd8e61070120db767
Andreas Knab <knab2ar@jmu.edu>
2011-07-06 09:37:14 -0400

    Fix user password reset in admin interface
    https://code.djangoproject.com/ticket/8916#comment:8

--- rooibos/userprofile/admin.py
+++ rooibos/userprofile/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from models import UserProfile
+from django.contrib.auth.admin import UserAdmin
 
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
 
-class UserAdmin(admin.ModelAdmin):
+class UserOptions(UserAdmin):
     inlines = [ UserProfileInline ]
 
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, UserOptions)
