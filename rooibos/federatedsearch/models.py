e574aba2a8c47b517609d038b081b5c00e2c6263
Andreas Knab <git config --global user.name "$git_name>
2011-10-11 13:44:14 -0400

    Add timeout to ARTstor federated search

--- rooibos/federatedsearch/models.py
+++ rooibos/federatedsearch/models.py
@@ -8,7 +8,7 @@ class CurrentHitCountManager(models.Manager):
 
 
 class HitCount(models.Model):
-    
+
     query = models.CharField(max_length=255, db_index=True)
     source = models.CharField(max_length=32, db_index=True)
     hits = models.IntegerField()
@@ -17,22 +17,25 @@ class HitCount(models.Model):
 
     objects = models.Manager()
     current_objects = CurrentHitCountManager()
-    
+
     def __unicode__(self):
         return "%s '%s': %s" % (self.source, self.query, self.hits)
-    
-    
+
+
 class FederatedSearch(object):
-    
+
+    def __init__(self, timeout=10):
+        super(FederatedSearch, self).__init__()
+        self.timeout = timeout
+
     def hits_count(self, query):
         raise NotImplementedError
-    
+
     def get_label(self):
         raise NotImplementedError
-    
+
     def get_source_id(self):
         raise NotImplementedError
-    
+
     def get_search_url(self):
         raise NotImplementedError
-    
