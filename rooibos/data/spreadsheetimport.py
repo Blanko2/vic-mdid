8868451d81e546e30424aec0ed6dd8edcb4b73d8
Andreas Knab <git config --global user.name "$git_name>
2011-11-17 11:13:28 -0500

    Force comma delimiter for CSV spreadsheet import

--- rooibos/data/spreadsheetimport.py
+++ rooibos/data/spreadsheetimport.py
@@ -66,6 +66,7 @@ class SpreadsheetImport(object):
         start = 3 if (self.csv_file.read(3) == "\xef\xbb\xbf") else 0  # skip BOM in some UTF-8 files
         self.csv_file.seek(start)
         dialect = csv.Sniffer().sniff(self.csv_file.read(65536))
+        dialect.delimiter = ','
         dialect.doublequote = True
         self.csv_file.seek(start)
         return csv.DictReader(self.csv_file, dialect=dialect)
