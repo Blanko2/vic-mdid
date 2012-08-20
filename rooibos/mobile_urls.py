d65b2eb18f3bcad746fee869d37325a05de6632c
silvalex <ss.alexandre.a@gmail.com>
2012-08-21 11:47:16 +1200

    Refactored code to connect to mobile module via url ("/mobile") as opposed to using mobile_middleware

--- rooibos/mobile_urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from rooibos.mobile.views import m_main
-
-urls = [
-    # needs SSL because of embedded login form, otherwise CSRF fails
-    url(r'^$', m_main, {'HELP': 'frontpage', 'SSL': True}, name='m_main')
-    ]
-
-urlpatterns = patterns('', *urls)
