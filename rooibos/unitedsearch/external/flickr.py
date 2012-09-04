f9cbddfc5f2dd5a25ba43100e15e77b57163658b
Max Zerzouri <maxdamantus@gmail.com>
2012-09-04 13:04:38 +1200

    Flickr selection through united search.

--- rooibos/unitedsearch/external/flickr.py
+++ rooibos/unitedsearch/external/flickr.py
@@ -13,17 +13,17 @@ identifier = "flickr"
 fed = FlickrSearch()
 fed._licenses = {}
 
-# search(searchobj) :: [(name, thumburl, fullurl)]
-#def _search(searchobj):
-#	return filter(lambda(t): all(map(lambda(v): v != None, t)), map(lambda(v): (v["title"], v["thumbnail_url"], v["object_url"]), searchobj["results"]))
-
 def search(term, params, off, len):
 	fs = fed.search(term, page=int(off/len if len > 0 else 0) + 1, pagesize=len)
 	result = Result(fs["hits"], off + len)
 	for i in fs["records"]:
-		result.addImage(Image(i["record_url"], i["thumb_url"], i["title"], i, json.dumps(i)))
+		result.addImage(ResultImage(i["record_url"], i["thumb_url"], i["title"], json.dumps(i)))
 	return result
 
 def getImage(identifier):
 	i = json.loads(identifier)
-	return Image(i["record_url"], i["thumb_url"], i["title"], i, identifier)
+	info = fed.flickr.flickr_call(method='flickr.photos.getSizes',
+					photo_id=i["flickr_id"],
+					format='xmlnode')
+	image_url = info.sizes[0].size[-1]['source']
+	return Image(image_url, i["thumb_url"], i["title"], i, identifier)
