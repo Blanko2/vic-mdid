262fa48ba8aee72f609fd6b67875e3df1dae6e30
silvalex <ss.alexandre.a@gmail.com>
2012-09-04 14:43:34 +1200

    Added m_search to mobile/views.py (uses united search and a copy of its template)

--- rooibos/mobile/views.py
+++ rooibos/mobile/views.py
@@ -4,10 +4,38 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.core.urlresolvers import reverse
 from django.views.decorators.csrf import csrf_protect
 
+from rooibos.unitedsearch import external
+from urllib import urlencode
+
 @csrf_protect
 def m_main(request):
     print '** blah!!'
     form = AuthenticationForm()
     request.session.set_test_cookie()
     return render_to_response('m_login.html', {'form': form}, context_instance=RequestContext(request))
+
+class usViewer():
+    def __init__(self, searcher):
+        self.searcher = searcher
+
+    def search(self, request):
+        query = request.GET.get('q', '') or request.POST.get('q', '')
+        offset = int(request.GET.get('from', '') or request.POST.get('from', '') or 0)
+        result = self.searcher.search(query, {}, offset, 6) # 50
+        results = result.images
+        return render_to_response('m_results.html',
+            {
+                'results': [ { 'thumb_url': i.thumb, 'title': i.name, 'record_url': i.infourl, 
+                              'identifier': i.identifier } for i in results ],
+                'select_url': reverse('united:%s:select' % self.searcher.identifier),
+                'next_page': reverse('united:%s:search' % self.searcher.identifier) + "?" + urlencode({ 'q': query, 'from': result.nextoffset }),
+                'hits': result.total,
+                'searcher_name': self.searcher.name
+            },
+            context_instance=RequestContext(request))
+        
+def m_search(request):
+    viewer = usViewer(external.flickr)
+    return viewer.search(request)
+        
 
