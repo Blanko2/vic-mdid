a7703827dd26da3b2624a5b9f5d58c54293c6d5a
silvalex <ss.alexandre.a@gmail.com>
2012-09-04 14:51:35 +1200

    Added results url to mobile/urls.py

--- rooibos/mobile/urls.py
+++ rooibos/mobile/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 from views import *
 
 urlpatterns = patterns('',
-    url(r'^$', m_main, name="mobile-main")
+    url(r'^$', m_main, name="mobile-main"),
+    url(r'search/results', m_search, name="mobile-search-results")
 )
