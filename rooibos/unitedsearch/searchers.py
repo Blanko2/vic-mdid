597501ec0458baf5c1b399137a355ca8add4da35
phillibrid1 <phillibrid1@ecs.vuw.ac.nz>
2012-09-04 12:47:30 +1200

    Initial (not working) implementation of Gallica

--- rooibos/unitedsearch/searchers.py
+++ rooibos/unitedsearch/searchers.py
@@ -1,8 +1,16 @@
+<<<<<<< OURS
 from external import digitalnz, nga, flickr
+=======
+from external import digitalnz, nga
+>>>>>>> THEIRS
 
 """ UnitedSearchers to search. Must have name and identifier attributes defined"""
+<<<<<<< OURS
 all = [
 	nga,
 	digitalnz,
 	flickr,
 ]
+=======
+all = [ digitalnz, nga ]
+>>>>>>> THEIRS
