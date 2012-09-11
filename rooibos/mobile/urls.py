f7bffea57c194df332a05a6863eb3a552ac41bb7
silvalex <ss.alexandre.a@gmail.com>
2012-09-11 13:45:34 +1200

    Added function to mobile/views to fill presentation browsing template; added url that links to the new function

--- rooibos/mobile/urls.py
+++ rooibos/mobile/urls.py
@@ -4,5 +4,6 @@ from views import *
 
 urlpatterns = patterns('',
     url(r'^$', m_main, name="mobile-main"),
-    url(r'search/results', m_search, name="mobile-search-results")
+    url(r'search/results', m_search, name="mobile-search-results"),
+    url(r'presentation/browse', m_browse, name="mobile-presentation-browse")
 )
