b35ead025467b47b8ecffa241d7e133ed7c738e9
Dean Woodward <bakeddean@gmail.com>
2012-10-18 12:55:34 +1300

    Added check boxes to select the databases to use (added redirect method in mobile views + new url).Made the presentation viewer work with ipod touch.

--- rooibos/mobile/urls.py
+++ rooibos/mobile/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     url(r'^$', m_main, name="mobile-main"),
     url(r'search/results', m_search, name="mobile-search-results"),
     url(r'presentation/browse', m_browse, name="mobile-presentation-browse"),
-    url(r'^search/(?P<sid>[a-z0-9A-Z,]*)/results', union_search, name="mobile-union-search")
+    url(r'^search/(?P<sid>[a-z0-9A-Z,]*)/results', union_search, name="mobile-union-search"),
+    url(r'^searchredirect/',redirect,name="mobile-search-redirect")
 )
