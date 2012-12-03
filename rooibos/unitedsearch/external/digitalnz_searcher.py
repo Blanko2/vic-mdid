53de933f727a162e6b5b4a4ea0079a195de1ccb6
Blanko2 <rodrigo.lomonaco@gmail.com>
2012-12-03 15:06:02 +1300

    finishing up dnz_searcher - modulates the offset to be the nearest possible round value (based on the per_page) meaning that it should compensate for crazy offset values

--- rooibos/unitedsearch/external/digitalnz_searcher.py
+++ rooibos/unitedsearch/external/digitalnz_searcher.py
@@ -9,39 +9,27 @@ name = "DigitalNZ"
 identifier = "digitalnz"
 
 API_KEY="sfypBYD5Jpu1XqYBipX8"
+CATEGORY_VALUE="&and[category][]=Images"
+
 BASE_IMAGE_LOCATION_URL="http://www.digitalnz.org/records?"
 BASE_METADATA_LOCATION_URL="http://api.digitalnz.org/v3/records/"
 BASE_SEARCH_API_URL="http://api.digitalnz.org/v3/records.rss?api_key="+API_KEY
+
 # TODO get a University API key instead of a personal one
 """
-NEED TO COMPLETELY REWRITE THIS CLASS SRSLY, NOT EVEN FUNNY
-    WHAT NEEDS TO BE DONE:
-        allow:
-        -- per_page
-        -- page
-        -- and[Category][]=Images 
 
 """
-def search(query, params, off, len):
-    """
-    off = int(off)
-    obj = __search(query, off, len)
-    hits = _count(obj)
-    result = Result(hits, off + len if off + len < hits else None)
-    """
+def search(query, params, offset, per_page):
     # build the URL 
-    url = _build_URL(query, params)
+    
+    offset = modulate_offset(offset, per_page)
+    page = offset/per_page +1 
+    url = _build_URL(query, params, per_page, page)
     return Result(0,0), params 
-    """
-    for i in obj["results"]:
-        u = i["object_url"] or i["large_thumbnail_url"] or None
-        # TODO: digitalnz's "Get Metadata API" doesn't seem to work---something better than using a JSON string as an identifier
-        result.addImage(ResultImage(i["source_url"], i["thumbnail_url"], i["title"], u and json.dumps(i)))
-    return result, {}
-    """
-def previousOffset(off, len):
-    off = int(off)
-    return off > 0 and str(off - len)
+
+def previousOffset(offset, per_page):
+    offset = int(offset)
+    return offset > 0 and str(offset - per_page)
 
 
 """
@@ -51,26 +39,29 @@ URL BUILDERS###########
 """
 """
 Builds the URL:
-    if there is anything inside params for the search it returns a complex URL - otherwise it returns a simple URL
+    there are only ever simple searches because digitalNZ has a weird search API that doesn't properly include its own filters
 """
-def _build_URL(query, params):
+def _build_URL(query, params, per_page, page):
+
     keywords, para_map = break_query_string(query) 
     url = ""
     # need to put all params into keywords because
     # dnz API accepts no useful parameters 
+    params = merge_dictionaries(para_map, params)[0]
     for p in params:
-        keywords+= " "+params[p]
-    url =  _build_simple_URL(keywords)
+        keywords+= "+"+params[p]
+    url =  _build_simple_URL(keywords, per_page, page)
     return url 
 
 
 """
-returns a simple search using the digitalNZ API
+returns a search using the digitalNZ API
 """
-def _build_simple_URL(keywords):
-    #TODO needs to do stuff.
-    url = BASE_SEARCH_API_URL+""
-    return 'a'
+def _build_simple_URL(keywords, per_page, page):
+    keywords = keywords.replace(" ","+")
+    url = BASE_SEARCH_API_URL+"&text="+keywords+CATEGORY_VALUE+"&per_page="+per_page+"&page="+page
+    return url 
+
 """
 ================
 #TOOLS
@@ -82,13 +73,30 @@ def get_image(identifier):
     u = i["object_url"] or i["large_thumbnail_url"]
     return Image(u, i["thumbnail_url"], i["title"], i, identifier)
 
-def check_valid_type(type):
+def modulate_offset(offset, per_page):
+    modulate = offset % per_page 
+    diff_mod = per-page - modulate
+   
+    if offset != 0 and modulate != 0:
+        #offset = Minimum Necessary Change between the offset and a modulo of zero
+        if modulate < diff_mod:
+            offset -= modulate
+        else:
+            offset += diff_mod 
+    # calculate the page
+    page = offset/per_page + 1
+    return offset, page
+
+"""
+OBSOLETE
+"""
+def _check_valid_type(type):
     if type in dnz_valid_types:    
         return True
     else:
         return False
 
-def check_valid_usage(usage):
+def _check_valid_usage(usage):
     if usage in dnz_valid_usage:
         return True
     else:
