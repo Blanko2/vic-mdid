367e16ed0770dafd555f32703ef255a7480637dc
Blanko2 <rodrigo.lomonaco@gmail.com>
2012-12-04 14:40:02 +1300

    digitalNZ_searcher should be completed

--- rooibos/unitedsearch/external/digitalnz_searcher.py
+++ rooibos/unitedsearch/external/digitalnz_searcher.py
@@ -13,6 +13,8 @@ CATEGORY_VALUE="&and[category][]=Images"
 
 BASE_IMAGE_LOCATION_URL="http://www.digitalnz.org/records?"
 BASE_METADATA_LOCATION_URL="http://api.digitalnz.org/v3/records/"
+END_METADATA_LOCATION_URL=".json?"+API_KEY
+
 BASE_SEARCH_API_URL="http://api.digitalnz.org/v3/records.rss?api_key="+API_KEY
 
 # TODO get a University API key instead of a personal one
@@ -20,14 +22,20 @@ BASE_SEARCH_API_URL="http://api.digitalnz.org/v3/records.rss?api_key="+API_KEY
 def search(query, params, offset, per_page=20):
     # build the URL 
     offset = _modulate_offset(int(offset), per_page)
+    nex_offset = offset+per_page
     page = offset/per_page +1 
-    url = _build_URL(query, params, per_page, page)
     
-    #result_images = Result(count(
-    # needs to have a result getting process and 
-    # needs to add images to that and return that
-    # result with the images
-    return params 
+    url = _build_URL(query, params, per_page, page)
+    result_object = _load(url) 
+    hits = count(query, parameters = params) 
+    result = Result(hits, next_offset if next_offset < hits else None) 
+    # add images
+    for object in result_objecti["results"]:
+        thumbnail_url = object["object_url"] or object["large_thumbnail_url"] or None 
+        image = ResultImage(object["source_url"], thumbnail_url, object["title"], object["id"])
+        result.addImage(image)
+    
+    return result,  params 
 
 # not worked on but should work anyway
 def previousOffset(offset, per_page):
@@ -35,10 +43,12 @@ def previousOffset(offset, per_page):
     return offset > 0 and str(offset - per_page)
 
 # count should be done
-def count(query):
+def count(query, parameters={}):
     # need to get result, get json that composes it and then
     # break that and get 'result_count' value
     keywords, params = break_query_string(query)
+    if parameters != {}:
+        params = merge_dictionaries(parameters, params, params.keys())
     search_object = _load(keywords, params) 
     hits = int(search_object["search"]["result_count"]) 
     
@@ -65,7 +75,6 @@ def _build_URL(query, params, per_page, page):
     url =  _build_simple_URL(keywords, per_page, page)
     return url 
 
-
 """
 returns a search url with all the given keywords, at the given page and with the number or specified results per page
 """
@@ -73,7 +82,6 @@ def _build_simple_URL(keywords, per_page, page):
     keywords = keywords.replace(" ","+")
     url = BASE_SEARCH_API_URL+"&text="+keywords+CATEGORY_VALUE+"&per_page="+str(per_page)+"&page="+str(page)
     return url 
-
 """
 ================
 #TOOLS
@@ -83,22 +91,29 @@ def _get_url(url):
     return urllib2.urlopen(url)
 
 """
-returns a json string with the result from the given url
+returns a python object from the resulting json string from the given url
 """
 def _load(url):
     return json.load(_get_url(url))
 
 """
-creates a url from a given query and loads the resulting json string
+creates a url from a given query and loads the resulting json string into a python object
 """
 def _load(keywords, params):
     # should build a url and return the json string that it returns
-    return json.load(_get_url(_build_URL(query, params, 20, 1)))
+    return json.load(_get_url(_build_URL(keywords, params, 20, 1)))
 
-def get_image(identifier):
-    i = json.loads(identifier)
-    u = i["object_url"] or i["large_thumbnail_url"]
-    return Image(u, i["thumbnail_url"], i["title"], i, identifier)
+
+# ====== GETTERS ========
+# =======================
+
+def getImage(identifier):
+    url = BASE_METADATA_LOCATION_URL+identifier+END_METADATA_LOCATION_URL
+    image_object = _load(url)['record'] 
+    location_url = image_object["object_url"] or image_object["large_thumbnail_url"]
+    thumbnail_url = image_object["thumbnail_url"]
+    title = image_object["title"]
+    return Image(location_url, thumbnail_url, title, image_object, identifier) 
 
 """
 modulates the offset to match a multiple of the page length -- if offset%per_page!=0 it changes to be the closest value
